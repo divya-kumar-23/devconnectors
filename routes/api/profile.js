8e1ed16776e5056ff48c4a643c2023ba9f71b350
unknown <kumard2312@gmail.com>
2019-01-21 23:38:32 +0530

    section 3 v18

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,14 @@
 const express=require('express');
-
 const router=express.Router();
+const mongoose=require('mongoose');
+const passport=require('passport');
+
+//load Profile Modal
+const Profile=require('../../models/Profile');
+
+//Load User Profile
+
+const User =require('../../models/User');
 
 
 //@route Get api/profile/test
@@ -8,5 +16,22 @@ const router=express.Router();
 //@access Public
 
 router.get('/test',(req,res)=>res.json({msg:"profile works"}));
+
+
+//@route Get api/profile
+//@desc Get current user profile
+//@access Private
+router.get('/', passport.authenticate('jwt',{session:false}),(req,res)=>{
+    const errors={};
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        if(!profile){
+            errors.npoprofile='there is no profile for this user';
+            return res.sendStatus(404).json();
+        }
+        res.json(profile);
+    })
+    .catch(err=>res.status(404).json(err));
+})
 
 module.exports=router;
